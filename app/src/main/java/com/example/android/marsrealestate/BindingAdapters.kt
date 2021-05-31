b7218e0d00a1345abd3426e51a50def41ea4f553
Zhiyun Li <zhiyun.li@gmail.com>
2021-05-31 11:42:37 -0700

    use Glide to display internet image.

--- app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -17,3 +17,21 @@
 
 package com.example.android.marsrealestate
 
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+/**
+ * The @BindingAdapter annotation tells data binding that you want this binding adapter
+ * executed when an XML item has the imageUrl attribute.
+ * */
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+imgUrl?.let{
+    val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+    Glide.with(imgView.context)
+        .load(imgUri)
+        .into(imgView)
+}
+}
