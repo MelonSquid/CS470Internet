3ca28702078375e55dbacfb58a93423d47ad775f
Zhiyun Li <zhiyun.li@gmail.com>
2021-05-31 11:44:43 -0700

    Show loading and error images.

--- app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -21,6 +21,7 @@ import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 /**
  * The @BindingAdapter annotation tells data binding that you want this binding adapter
@@ -28,10 +29,15 @@ import com.bumptech.glide.Glide
  * */
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
-imgUrl?.let{
-    val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
-    Glide.with(imgView.context)
-        .load(imgUri)
-        .into(imgView)
-}
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .apply(
+                RequestOptions()
+                    .placeholder(R.drawable.loading_animation)
+                    .error(R.drawable.ic_broken_image)
+            )
+            .into(imgView)
+    }
 }
