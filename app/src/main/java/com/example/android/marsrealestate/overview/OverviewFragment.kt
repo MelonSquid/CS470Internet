4dcb85fb466d25fe425760a283bc68e3982faef5
Zhiyun Li <zhiyun.li@gmail.com>
2021-06-01 20:02:33 -0700

    connect adapter with data and RecyclerView.

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
@@ -20,6 +20,7 @@ package com.example.android.marsrealestate.overview
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.marsrealestate.R
 import com.example.android.marsrealestate.databinding.FragmentOverviewBinding
@@ -51,6 +52,15 @@ class OverviewFragment : Fragment() {
 
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
+
+        val adapter = PhotoGridAdapter()
+        binding.photosGrid.adapter = adapter
+
+        viewModel.properties.observe(viewLifecycleOwner, Observer {
+            it?.let{
+                adapter.submitList(it)
+            }
+        })
 
         setHasOptionsMenu(true)
         return binding.root
